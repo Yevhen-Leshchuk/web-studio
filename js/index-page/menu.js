fe59624456eb0f53d4546b8adf63cb2b8479c48b
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2021-10-10 19:43:25 +0300

    added fixes

--- js/index-page/menu.js
+++ js/index-page/menu.js
@@ -11,14 +11,17 @@ function onOpenMenu(event) {
   // mobileMenuRef.classList.add("is-open");
   // menuBtnClose.classList.add("is-open");
 
-  // refs.bodyRef.classList.add('is-hidden');
+  // refs.bodyRef.classList.add(
+  //   'body_landscape--hidden',
+  //   'body_vertical--hidden',
+  // );
   // refs.htmlRef.classList.add('is-landscape');
 
   refs.menuBtnOpen.classList.toggle('is-open');
   refs.mobileMenuRef.classList.toggle('is-open'); // toggle добавляет класс если его нет и удаляет если он есть
   refs.menuBtnClose.classList.toggle('is-open');
 
-  refs.bodyRef.classList.toggle('is-hidden');
+  refs.bodyRef.classList.add('body_landscape--hidden', 'body_vertical--hidden');
   refs.htmlRef.classList.toggle('is-landscape');
 }
 
@@ -27,14 +30,20 @@ function onCloseMenu() {
   // mobileMenuRef.classList.remove("is-open");
   // menuBtnClose.classList.remove("is-open");
 
-  // refs.bodyRef.classList.add('is-hidden');
-  // refs.htmlRef.classList.add('is-landscape');
+  // refs.bodyRef.classList.remove(
+  //   'body_landscape--hidden',
+  //   'body_vertical--hidden',
+  // );
+  // refs.htmlRef.classList.remove('is-landscape');
 
   refs.menuBtnOpen.classList.toggle('is-open');
   refs.mobileMenuRef.classList.toggle('is-open'); // toggle добавляет класс если его нет и удаляет если он есть
   refs.menuBtnClose.classList.toggle('is-open');
 
-  refs.bodyRef.classList.toggle('is-hidden');
+  refs.bodyRef.classList.remove(
+    'body_landscape--hidden',
+    'body_vertical--hidden',
+  );
   refs.htmlRef.classList.toggle('is-landscape');
 }
 
--- js/portfolio-page/menu.js
+++ js/portfolio-page/menu.js
@@ -25,7 +25,7 @@ function onOpenMenu(event) {
   refs.mobileMenuRef.classList.toggle('is-open'); // toggle добавляет класс если его нет и удаляет если он есть
   refs.menuBtnClose.classList.toggle('is-open');
 
-  refs.bodyRef.classList.toggle('is-hidden');
+  refs.bodyRef.classList.add('body_landscape--hidden', 'body_vertical--hidden');
   refs.htmlRef.classList.toggle('is-landscape');
 }
 
@@ -34,14 +34,20 @@ function onCloseMenu() {
   // mobileMenuRef.classList.remove("is-open");
   // menuBtnClose.classList.remove("is-open");
 
-  // refs.bodyRef.classList.add('is-hidden');
+  // refs.bodyRef.classList.add(
+  //   'body_landscape--hidden',
+  //   'body_vertical--hidden',
+  // );
   // refs.htmlRef.classList.add('is-landscape');
 
   refs.menuBtnOpen.classList.toggle('is-open');
   refs.mobileMenuRef.classList.toggle('is-open'); // toggle добавляет класс если его нет и удаляет если он есть
   refs.menuBtnClose.classList.toggle('is-open');
 
-  refs.bodyRef.classList.toggle('is-hidden');
+  refs.bodyRef.classList.remove(
+    'body_landscape--hidden',
+    'body_vertical--hidden',
+  );
   refs.htmlRef.classList.toggle('is-landscape');
 }
 
